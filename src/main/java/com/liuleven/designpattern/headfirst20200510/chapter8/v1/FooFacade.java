f417d8cf44397ba14d479e41e30b5d84c4604036
liuziqing <liueleven@aliyun.com>
2020-06-06 23:40:08 +0800

    《Head First设计模式》门面模式代码

--- src/main/java/com/liuleven/designpattern/headfirst20200510/chapter8/v1/FooFacade.java
+++ src/main/java/com/liuleven/designpattern/headfirst20200510/chapter8/v1/FooFacade.java
@@ -1,7 +1,7 @@
 package com.liuleven.designpattern.headfirst20200510.chapter8.v1;
 
 /**
- * @description: 一定要写注释啊
+ * @description: 门面模式虽然简单，但还是要遵守单一原则，但也没必要太琐碎，具体情况具体分析
  * @date: 2020-06-06 20:12
  * @author: 十一
  */
