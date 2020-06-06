5d6ca74f26ca2cadd07aa46499fa47797c665e3b
liuziqing <liueleven@aliyun.com>
2020-06-06 20:13:08 +0800

    《Head First设计模式》门面模式代码

---
+++ src/main/java/com/liuleven/designpattern/headfirst20200510/chapter8/v1/FooFacade.java
@@ -0,0 +1,11 @@
+package com.liuleven.designpattern.headfirst20200510.chapter8.v1;
+
+/**
+ * @description: 一定要写注释啊
+ * @date: 2020-06-06 20:12
+ * @author: 十一
+ */
+public interface FooFacade {
+
+    void doSoming();
+}
---
+++ src/main/java/com/liuleven/designpattern/headfirst20200510/chapter8/v1/FooFacadeImpl.java
@@ -0,0 +1,13 @@
+package com.liuleven.designpattern.headfirst20200510.chapter8.v1;
+
+/**
+ * @description: 一定要写注释啊
+ * @date: 2020-06-06 20:12
+ * @author: 十一
+ */
+public class FooFacadeImpl implements FooFacade {
+    @Override
+    public void doSoming() {
+        System.out.println("do soming...");
+    }
+}
